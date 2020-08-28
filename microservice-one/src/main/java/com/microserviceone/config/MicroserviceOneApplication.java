6a4a8697fef326e6dfd26b789f28f21333d25bef
MazinIsmailW <MA20011300@APL-31R4KW2>
2020-08-28 12:37:50 +0530

    Clean up

--- microservice-one/src/main/java/com/microserviceone/config/MicroserviceOneApplication.java
+++ microservice-one/src/main/java/com/microserviceone/config/MicroserviceOneApplication.java
@@ -11,16 +11,16 @@ import org.springframework.web.client.RestTemplate;
  * Spring cloud Sleuth logs structure is as follows
  * [application-name,traceid,spanid,zipkin-export]
  * 
- * Application-name = Name of the application 
+ * Application-name = Name of the application
  * 
- * Traceid = each request and response traceid is same when calling
- *  same service or one service to another service.
+ * Traceid = each request and response traceid is same when calling same service
+ * or one service to another service.
  * 
- * Spanid = Span Id is printed along with Trace Id. Span Id is different
- * every request and response calling one service to another service.
+ * Spanid = Span Id is printed along with Trace Id. Span Id is different every
+ * request and response calling one service to another service.
  * 
  * Zipkin-export = By default it is false. If it is true, logs will be exported
- * to the Zipkin server.*
+ * to the Zipkin server.
  *
  */
 @SpringBootApplication
